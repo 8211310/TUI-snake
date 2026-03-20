c445119b71a9ffd3d448c1201dab238cf871e469
Xu8211 <8211310@qq.com>
2026-03-20 18:26:54 +0800

    Finish the shift from a Single Snake() to multiple functions and structs passing arguments.

--- Snake.c
+++ Snake.c
@@ -1,7 +1,15 @@
 #include "snake.h"
 
 
+
 void errorMessage(char * input){
+	/*Yes, if you want to debug, just write 
+	 * this function, It will print debug
+	 * info to the middle of the screen and
+	 * wait for your response.
+	 * Just like printf. Genius idea.*/
+
+
 	titleString title = createString("Error!");
 	titleString message = createString(input);
 	attron(COLOR_PAIR(COLOR_RED));
@@ -20,11 +28,6 @@ void errorMessage(char * input){
 
 
 
-typedef struct snakeNode{
-	int x;
-	int y;
-	struct snakeNode *next;
-}node;
 
 
 typedef struct structWindow{
@@ -32,20 +35,185 @@ typedef struct structWindow{
 	int width;
 	int startx;
 	int starty;
-	WINDOW *win;
+	int color;	/*The color is theme color.*/ 
+	WINDOW *win;	/*Main window part.*/
 }window;
 
 void initWindowParameters(window *mainGame, window *menu){
+	/*fixed width*/
 	menu->width = 40;
-	mainGame->height = LINES - 2*2;
-	mainGame->width = COLS - 3*2 - menu_width;
+
+
+	menu->height = mainGame->height = LINES - 2*2;
+	mainGame->width = COLS - 3*2 - menu->width;
+	
 	mainGame->startx = mainGame->starty = 3;
+	
+	menu->startx = mainGame->startx;
+	menu->starty = mainGame->starty+mainGame->width;
+
+	/*theme*/
+	mainGame->color = COLOR_YELLOW;
+	menu->color = COLOR_GREEN;
 }
 
 void createWindow(window *param){
 	param->win = newwin(param->height, param->width, param->startx, param->starty);
 }
 
+
+void drawWindow(window *param, char* title){ 
+
+	wattron(param->win, COLOR_PAIR(param->color));
+	box(param->win,0,0);
+	mvwprintw(param->win, 0, 2, "%s", title);
+	wattroff(param->win, COLOR_PAIR(param->color));
+	wrefresh(param->win);	
+
+}
+
+
+
+
+/*snake part*/
+
+typedef struct snakeNode{
+	int x;
+	int y;
+	struct snakeNode *next;
+}node;
+
+
+typedef struct structSnake{
+
+	char body;
+	
+	int head_color;
+	int body_color;
+
+	int length;
+
+	node *head;
+	node *tail;
+}snake;
+
+void createSnake(snake* param, window *mainGame);
+void initDrawSnake(snake* param, window *mainGame);
+
+void initSnakeParam(snake* param, window *mainGame){
+	param -> body = '@';
+
+	param -> head_color = COLOR_RED;
+	param -> body_color = COLOR_CYAN;
+
+	param -> length = 9;
+
+	/*Check if the set is valid*/
+	if(param->length > mainGame->width -2){
+		errorMessage("init_length is longer than the width of screen.");
+#ifdef EXIT_WITH_ERRORS
+		exit(1);
+#endif
+	}
+
+	createSnake(param, mainGame);
+	initDrawSnake(param, mainGame);
+
+
+}
+
+
+void createSnake(snake* param, window *mainGame){
+
+	/*Creating Snake*/
+	node* tmp_head = malloc(sizeof (node));
+	if (!tmp_head){
+		errorMessage("malloc head node failed!");
+	}
+	tmp_head->x = mainGame->height /2;
+	tmp_head->y = mainGame->width /2 + param->length/2;
+	node *tmp_last = tmp_head;
+	for(int i = 1; i < param->length ; i++){
+		node *tmp_node = malloc(sizeof (node));
+		if(!tmp_node){
+			errorMessage("malloc body node failed!");
+		}
+		tmp_node -> x = tmp_head ->x;
+		tmp_node -> y = tmp_head ->y - i;
+		tmp_last -> next = tmp_node;
+		tmp_last = tmp_node;
+	}
+	tmp_last -> next = NULL;
+
+	param->head = tmp_head;
+	param->tail = tmp_last;
+}
+
+void initDrawSnake(snake* param, window *mainGame){
+
+	node *tmp_last = param->head;
+	int if_head = 1;
+	while(1){
+		if(if_head) wattron(mainGame->win, COLOR_PAIR(param->head_color));
+		else wattron(mainGame->win, COLOR_PAIR(param->body_color));
+		mvwprintw(mainGame->win, tmp_last->x,tmp_last->y, "%c", param->body);
+		if(if_head){
+			wattroff(mainGame->win, COLOR_PAIR(param->head_color));
+			if_head = 0;
+		}
+		else wattroff(mainGame->win, COLOR_PAIR(param->body_color));
+		if(tmp_last -> next){
+			tmp_last = tmp_last -> next;
+		}else{
+			break;
+		}
+	}
+
+}
+
+
+void initCreateMenu(titleString *menuList, int listNum){
+	menuList[0] = createString("Snake Length:");
+	menuList[1] = createString("Level ");
+	menuList[2] = createString("Time using:");
+}
+
+void initDrawMenu(titleString *menuList, int listNum, window *menu, snake *mainSnake){
+	wattron(menu->win, COLOR_PAIR(menu->color));
+	wmove(menu->win,3,0);
+	for(int i =0; i < menu->height/6; i++){
+		wprintw(menu->win, "\n");
+	}
+
+	int menu_list_width = 2;
+	for(int i = 0; i < listNum; i++){
+		wprintw(menu->win, "%s\n", menuList[i].str);
+		for(int j = 0; j < menu_list_width; j++){
+			wprintw(menu->win, "\n");
+		}
+	}
+
+	wattroff(menu->win, COLOR_PAIR(menu->color));
+
+	wrefresh(menu->win);
+}
+void refreshMenuParam(titleString *menuList, int listNum, window *menu, snake *mainSnake){
+	
+	int color_of_length = COLOR_RED;
+	wattron(menu->win, COLOR_PAIR(color_of_length));
+	mvwprintw(menu->win, 2+menu->height/6, menuList[0].len, "%d", mainSnake->length);
+	wattroff(menu->win, COLOR_PAIR(color_of_length));
+
+	int color_of_level = COLOR_YELLOW;
+	wattron(menu->win, COLOR_PAIR(color_of_level));
+	mvwprintw(menu->win, 3+2+menu->height/6, menuList[1].len, "%d", 1);
+	wattroff(menu->win, COLOR_PAIR(color_of_level));
+
+	int color_of_time = COLOR_BLUE;
+	wattron(menu->win, COLOR_PAIR(color_of_time));
+	//mvwprintw(menu->win,3+3+2+height/6, menuList[2].len, "%d:%d", time->min, time->sec);
+	wattroff(menu->win, COLOR_PAIR(color_of_time));
+}
 
 
 int Snake(){
@@ -57,119 +225,50 @@ int Snake(){
 	window menuEntity;
 	window *menu = &menuEntity;
 
-	initWindowParameters(&mainGame, &menu);
+	initWindowParameters(mainGame, menu);
 	
 
 	createWindow(mainGame);
+	drawWindow(mainGame, "Snake Game");
 	createWindow(menu);
-
-
-	int theme_color = COLOR_YELLOW;
-	wattron(mainGame, COLOR_PAIR(theme_color));
-	box(mainGame,0,0);
-	mvwprintw(mainGame, 0, 2, "Snake Game");
-	wattroff(mainGame, COLOR_PAIR(theme_color));
-	wrefresh(mainGame);	
-	
-
-	/*Define Menu*/
-	WINDOW* menu = newwin(height, menu_width, startx, starty+width+1);
-	int menu_color = COLOR_GREEN;
+	drawWindow(menu, "Score");
 
 
 
-
-	/*Basic set of snake*/
-	char Snake_body = '@';
-	
-	int Snake_head_color = COLOR_RED;
-	int Snake_body_color = COLOR_CYAN;
-
-	int init_length = 9;
+	snake *mainSnake = malloc(sizeof (snake));
+	initSnakeParam(mainSnake, mainGame);
 
 
-	/*Check if the set is valid*/
-	if(init_length > width -2){
-		errorMessage("init_length is longer than the width of screen.");
-#ifdef EXIT_WITH_ERRORS
-		exit(1);
-#endif
-	}
+	/*Create Menu*/
+	int listNum = 3;
+	titleString menuList[listNum];
+	initCreateMenu(menuList, listNum);
+	initDrawMenu(menuList, listNum, menu, mainSnake);
+	//refreshMenuParam(menuList, listNum, menu, mainSnake);
 
 
 	/*Draw Menu*/
-	wattron(menu, COLOR_PAIR(menu_color));
-	wmove(menu,2,0);
-	wprintw(menu,"Score Board");
-	for(int i =0; i < height/6; i++){
-		wprintw(menu, "\n");
-	}
-	titleString snakeLength = createString("Snake Length:");
-	titleString level = createString("Level ");
-	wprintw(menu, "%s", snakeLength.str);
-	int menu_list_width = 2;
-	for(int i = 0; i < menu_list_width; i++){
-		wprintw(menu, "\n");
-	}
-	wattroff(menu, COLOR_PAIR(menu_color));
-
-	wattron(menu, COLOR_PAIR(COLOR_RED));
-	mvwprintw(menu, 2+height/6, snakeLength.len, "%d", init_length);
-	wattroff(menu, COLOR_PAIR(COLOR_RED));
-
-	wrefresh(menu);
-	/*Creating Snake*/
-	node *head = malloc(sizeof (node));
-	head->x = height /2;
-	head->y = width /2 + init_length/2;
-	node *tmp_last = head;
-	for(int i = 1; i < init_length ; i++){
-		node *tmp_node = malloc(sizeof (node));
-		tmp_node -> x = head ->x;
-		tmp_node -> y = head ->y - i;
-		tmp_last -> next = tmp_node;
-		tmp_last = tmp_node;
-	}
-	tmp_last -> next = NULL;
-
 	/*Draw Snake*/
-	tmp_last = head;
-	int if_head = 1;
-	while(1){
-		if(if_head) wattron(mainGame, COLOR_PAIR(Snake_head_color));
-		else wattron(mainGame, COLOR_PAIR(Snake_body_color));
-		mvwprintw(mainGame, tmp_last->x,tmp_last->y, "%c", Snake_body);
-		if(if_head){
-			wattroff(mainGame, COLOR_PAIR(Snake_head_color));
-			if_head = 0;
-		}
-		else wattroff(mainGame, COLOR_PAIR(Snake_body_color));
-		if(tmp_last -> next){
-			tmp_last = tmp_last -> next;
-		}else{
-			break;
-		}
-	}
-	node *tail = tmp_last;
 
 	/*Draw How to play*/
-	wattron(mainGame, COLOR_PAIR(theme_color));
+	wattron(mainGame->win, COLOR_PAIR(mainGame->color));
 	titleString help= createString("Use w,a,s,d as directions to play");
-	mvwprintw(mainGame, height*2/3, (width - help.len)/2, "%s", help.str);
-	wattroff(mainGame, COLOR_PAIR(theme_color));
-	wrefresh(mainGame);
+	mvwprintw(mainGame->win, mainGame->height*2/3, (mainGame->width - help.len)/2, "%s", help.str);
+	wattroff(mainGame->win, COLOR_PAIR(mainGame->color));
+	wrefresh(mainGame->win);
 	getch();
 
 
 	/*Main game*/
 	char directions[4] = {'w','a','s','d'};
-	char direction = 'd'
+	char direction = 'd';
 	int delay = 300;
+	/*
 	while(1){
 		timeout(delay);
 		switch(getch()){
 			case direction:
-				
+	*/			
 	return 0;
 }
 
